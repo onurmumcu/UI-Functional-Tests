3ea0b93a66893d3a5aedb071e35e0f129476374d
Selenium <Selenium@VAIO>
2018-05-07 19:10:11 -0400

    first commit and push

---
+++ src/com/tests/TableTests.java
@@ -0,0 +1,45 @@
+package com.tests;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TableTests {
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+//		System.setProperty("webdriver.gecko.driver", TestConstants.FIREFOX_PATH);
+//		WebDriver driver = new FirefoxDriver();
+		
+		System.setProperty("webdriver.chrome.driver", TestConstants.CHROME_PATH);
+		driver = new ChromeDriver();
+		
+		driver.get(TestConstants.WEB_ORDERS_URL);
+		driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+		driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+Keys.ENTER);
+		
+		
+
+}
+	
+	@Test
+	public void test() {
+		driver.findElement(By.linkText("View all products")).click();
+		
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+	
+	
+}
