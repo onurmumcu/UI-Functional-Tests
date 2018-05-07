391595a071cf1f50bee1df527f9688b9371318ea
Selenium <Selenium@192.168.1.13>
2018-05-07 19:35:29 -0400

    added mac chrome path

--- src/com/tests/TestConstants.java
+++ src/com/tests/TestConstants.java
@@ -1,8 +1,8 @@
 package com.tests;
 
 public class TestConstants {
-	public static final String CHROME_PATH = "C:\\Users\\Selenium\\Documents\\selenium dependencies\\drivers\\chromedriver.exe";
+	public static final String WIN_CHROME_PATH = "C:\\Users\\Selenium\\Documents\\selenium dependencies\\drivers\\chromedriver.exe";
 	public static final String WEB_ORDERS_URL ="http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx";
-	public static final String FIREFOX_PATH = "C:\\Users\\Selenium\\Documents\\selenium dependencies\\drivers\\geckodriver.exe";
-
+	public static final String WIN_FIREFOX_PATH = "C:\\Users\\Selenium\\Documents\\selenium dependencies\\drivers\\geckodriver.exe";
+	public static final String MAC_CHROME_PATH = "/Users/Selenium/Documents/selenium dependencies/drivers";
 }
