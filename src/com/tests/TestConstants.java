9ad843d8be5b720853ff3afd34686920e6ff9081
Selenium <Selenium@VAIO>
2018-05-07 19:20:57 -0400

    second commit

---
+++ src/com/tests/TestConstants.java
@@ -0,0 +1,8 @@
+package com.tests;
+
+public class TestConstants {
+	public static final String CHROME_PATH = "C:\\Users\\Selenium\\Documents\\selenium dependencies\\drivers\\chromedriver.exe";
+	public static final String WEB_ORDERS_URL ="http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx";
+	public static final String FIREFOX_PATH = "C:\\Users\\Selenium\\Documents\\selenium dependencies\\drivers\\geckodriver.exe";
+
+}
