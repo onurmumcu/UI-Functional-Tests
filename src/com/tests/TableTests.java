59f2cacc26b534b5fdaa84ba561995716b2833d3
Selenium <Selenium@192.168.1.13>
2018-05-07 22:36:40 -0400

    maximize window

--- src/com/tests/TableTests.java
+++ src/com/tests/TableTests.java
@@ -1,9 +1,12 @@
 package com.tests;
 
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -21,6 +24,7 @@ public class TableTests {
 		System.setProperty("webdriver.chrome.driver", TestConstants.MAC_CHROME_PATH);
 		driver = new ChromeDriver();
 		
+		driver.manage().window().maximize();
 		driver.get(TestConstants.WEB_ORDERS_URL);
 		driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
 		driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+Keys.ENTER);
@@ -33,12 +37,23 @@ public class TableTests {
 	public void test() {
 		driver.findElement(By.linkText("View all products")).click();
 		
+		//Print the whole table
+		WebElement table = driver.findElement(By.xpath("//table[@class='ProductsTable']"));
+		System.out.println(table.getText());
 		
+		//Print the names of the columns
+		List<WebElement> columnNames = driver.findElements(By.xpath("//table[@class='ProductsTable']//th"));
+		for (WebElement columnName : columnNames) {
+			System.out.println(columnName.getText());
+			
+		}
+			
+	
 	}
 	
 	@AfterMethod
 	public void tearDown() {
-		driver.close();
+		//driver.close();
 	}
 	
 	
